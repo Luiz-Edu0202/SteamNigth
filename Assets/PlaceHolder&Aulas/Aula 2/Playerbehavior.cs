fdd2375788f0c0e56a6eaf6951c981701739ae91
Luiz-Edu0202 <Brayner.luizeduardo@gmail.com>
2019-08-20 09:49:28 -0300

    Versão 0.1

---
+++ Assets/PlaceHolder&Aulas/Aula 2/Playerbehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playerbehavior : MonoBehaviour
+{
+    [SerializeField] private float StandartSpeed;
+    [SerializeField] private float StandartJump;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            StandartJump = 5;
+        }
+        else
+        {
+            StandartJump = 0;
+        }
+         rb.velocity = new Vector2(Input.GetAxis("Horizontal")* StandartSpeed,StandartJump);
+    }
+}
---
+++ Assets/PlaceHolder&Aulas/Aula1/MouseBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseBehavior : MonoBehaviour
+{
+    public GameObject Prefab;
+    //Prefab do Circulo que será instansiado
+    Vector3 mousePos;
+    //Posição do mouse na tela
+    Vector2 mousePos2D;
+    //Posição do mouse no jogo
+    void Update()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //Esse comando converte a posição do mouse da tela para a do jogo
+        mousePos.z = 0;
+        //Esse comando seta o z para 0, pois a posição do z não é convertida antes
+        if (Input.GetMouseButtonDown(0))
+        {
+           Instantiate(Prefab,mousePos,Quaternion.identity);
+           //Esse comando instancia a bolo
+        }
+        //Essa condicional captura se o mouse foi apertado  
+    }
+}
