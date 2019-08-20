398b80614e3013671b331667be457a7603b28321
Luiz-Edu0202 <Brayner.luizeduardo@gmail.com>
2019-08-20 12:30:02 -0300

    Versão 0.2

---
+++ Assets/PlaceHolder&Aulas/Aula 3/TouchBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchBehavior : MonoBehaviour
+{
+    [SerializeField] private GameObject Obj;
+    Vector3 mousePos;
+    //Posição do mouse na tela
+    Vector2 touchPos;
+    //Posição do mouse na tela
+    Touch touch;
+    void Update()
+    {
+#if UNITY_EDITOR || UNITY_STANDALONE
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //Esse comando converte a posição do mouse da tela para a do jogo
+        mousePos.z = 0;
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+           Instantiate(Obj,mousePos,Quaternion.identity);
+           //Esse comando instancia a bolo
+        }
+        //Essa condicional captura se o mouse foi apertado 
+#elif UNITY_ANDROID
+        if(Input.touchCount > 0)
+        {
+            touch = Input.Touches[0];
+            If(touch.phase == TouchPhase.Began)
+            {
+                touchPos =  Camera.main.ScreenToWorldPoint(touch.position);
+                Instantiate(Obj,touchPos,Quaternion.identity);
+            }
+           
+        }
+#endif
+    }
+}
