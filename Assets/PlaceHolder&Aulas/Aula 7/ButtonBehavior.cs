64b61fdb283f2ca3ba0f8457f5f7739797089411
Luiz-Edu0202 <Brayner.luizeduardo@gmail.com>
2019-10-22 12:10:33 -0300

    Versão 0.6

---
+++ Assets/PlaceHolder&Aulas/Aula 7/ButtonBehavior.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ButtonBehavior : MonoBehaviour
+{
+    public bool varivavel = true;
+
+    [SerializeField] private Sprite spritePositiveOfButton;
+    [SerializeField] private Sprite spriteNegativeOfButton;
+    [SerializeField] private Sprite spriteNullOfButton;
+    public Button button;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        ChangeTheSprite();
+    }
+
+    public void ChangeTheScene()
+    {
+        SceneManager.LoadScene("Fim");
+    }
+     public void ChangeTheValue()
+    {
+        if(varivavel == true)
+        {
+            varivavel = false;
+        }
+        else if (varivavel == false)
+        {
+            varivavel = true;
+        }
+    }
+    private void ChangeTheSprite()
+    {
+        if(varivavel == true)
+        {
+            button.image.sprite = spritePositiveOfButton;
+        }
+        else if (varivavel == false)
+        {
+            button.image.sprite = spriteNegativeOfButton;
+
+        }
+        else
+        {
+            button.image.sprite = spriteNullOfButton;
+
+        }
+    }
+}
---
+++ Assets/PlaceHolder&Aulas/Aula 7/ThiefBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ThiefBehavior : MonoBehaviour
+{
+    [SerializeField] private bool value;
+    [SerializeField] private GameObject buttono;
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        if(SceneManager.sceneCount == 0)
+        {
+            buttono = GameObject.FindGameObjectWithTag("LALA");
+        }
+    }
+
+    void Update()
+    {
+        value = buttono.GetComponent<ButtonBehavior>().varivavel;
+    }
+}
