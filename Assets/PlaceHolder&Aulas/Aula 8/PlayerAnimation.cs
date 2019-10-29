f7de0cc35255374a555861b487f75b8805941b27
Leonardo Viana <leonardoviana.nave@gmail.com>
2019-10-29 11:59:48 -0300

    Versão 0.8

---
+++ Assets/PlaceHolder&Aulas/Aula 8/PlayerAnimation.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    public Animator PlayerAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovimentate();
+    }
+
+    void PlayerMovimentate()
+    {
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            PlayerAnimator.SetBool("Up", false);
+            PlayerAnimator.SetBool("Left", false);
+            PlayerAnimator.SetBool("Rigth", true);
+            PlayerAnimator.SetBool("Idle", false);
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            PlayerAnimator.SetBool("Up", false);
+            PlayerAnimator.SetBool("Left", true);
+            PlayerAnimator.SetBool("Rigth", false);
+            PlayerAnimator.SetBool("Idle", false);
+        }
+        else if (Input.GetAxisRaw("Vertical") > 0)
+        {
+            PlayerAnimator.SetBool("Up", true);
+            PlayerAnimator.SetBool("Left", false);
+            PlayerAnimator.SetBool("Rigth", false);
+            PlayerAnimator.SetBool("Idle", false);
+        }
+        else if (Input.GetAxisRaw("Vertical") < 0)
+        {
+            PlayerAnimator.SetBool("Up", false);
+            PlayerAnimator.SetBool("Left", false);
+            PlayerAnimator.SetBool("Rigth", false);
+            PlayerAnimator.SetBool("Idle", false);
+        }
+        else if (Input.GetAxisRaw("Vertical") == 0 && Input.GetAxisRaw("Horizontal") == 0)
+        {
+            PlayerAnimator.SetBool("Up", false);
+            PlayerAnimator.SetBool("Left", false);
+            PlayerAnimator.SetBool("Rigth", false);
+            PlayerAnimator.SetBool("Idle", true);
+        }
+    }
+}
---
+++ Assets/PlaceHolder&Aulas/Aula 8/Playerbehavior2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playerbehavior2 : MonoBehaviour
+{
+    [SerializeField] private float StandartSpeed;
+    [SerializeField] private float StandartJump;
+    Rigidbody2D rb;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            rb.velocity = new Vector2(Input.GetAxis("Horizontal")* StandartSpeed * 10, 0);
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            rb.AddForce(new Vector2(0,StandartJump * 1000));
+        }
+    }
+}
